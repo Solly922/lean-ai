34b5d90625de97cf383504b7f3041ed6986861c3
Solomon Schuster <solomon.schuster01@gmail.com>
2025-01-03 14:26:19 -0700

    hides video

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -4,7 +4,7 @@ import ModalVideo from "@/components/modal-video";
 export default function HeroHome() {
   return (
     <section>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="px-4 mx-auto max-w-6xl sm:px-6">
         {/* Hero content */}
         <div className="py-12 md:py-20">
           {/* Section header */}
@@ -24,21 +24,27 @@ export default function HeroHome() {
                 Our landing page template works on all devices, so you only have
                 to set it up once, and get beautiful results forever.
               </p>
-              <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
-                <div data-aos="fade-up" data-aos-delay={400}>
+              <div className="mx-auto max-w-xs sm:flex sm:justify-center sm:max-w-none">
+                <div
+                  data-aos="fade-up"
+                  data-aos-delay={400}
+                >
                   <a
                     className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
                     href="#0"
                   >
-                    <span className="relative inline-flex items-center">
+                    <span className="inline-flex relative items-center">
                       Start Building
-                      <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
+                      <span className="ml-1 tracking-normal transition-transform group-hover:translate-x-0.5 text-white/50">
                         -&gt;
                       </span>
                     </span>
                   </a>
                 </div>
-                <div data-aos="fade-up" data-aos-delay={600}>
+                <div
+                  data-aos="fade-up"
+                  data-aos-delay={600}
+                >
                   <a
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
                     href="#0"
@@ -50,15 +56,15 @@ export default function HeroHome() {
             </div>
           </div>
 
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={1104}
-            thumbHeight={576}
-            thumbAlt="Modal video thumbnail"
-            video="videos//video.mp4"
-            videoWidth={1920}
-            videoHeight={1080}
-          />
+          {/* <ModalVideo */}
+          {/*   thumb={VideoThumb} */}
+          {/*   thumbWidth={1104} */}
+          {/*   thumbHeight={576} */}
+          {/*   thumbAlt="Modal video thumbnail" */}
+          {/*   video="videos//video.mp4" */}
+          {/*   videoWidth={1920} */}
+          {/*   videoHeight={1080} */}
+          {/* /> */}
         </div>
       </div>
     </section>
